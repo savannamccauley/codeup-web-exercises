a9cfd681606831f03c5b1c22419f842edf3b083e
Savanna McCauley <savannamccauley@yahoo.com>
2023-03-29 10:10:01 -0500

    jquery-faq

--- js/jquery.js
+++ js/jquery.js
@@ -1,15 +1,15 @@
-$('h1').click(function(){
-    $(this).css("background-color", "pink")
-})
-$('p').dblclick(function(){
-    $(this).css("font-size","18px");
-})
-
-$('li').hover(
-    function() {
-        $(this).css("color", "pink");
-    },
-    function(){
-        $(this).css("color", "black");
-
-})
+// $('h1').click(function(){
+//     $(this).css("background-color", "pink")
+// })
+// $('p').dblclick(function(){
+//     $(this).css("font-size","18px");
+// })
+//
+// $('li').hover(
+//     function() {
+//         $(this).css("color", "pink");
+//     },
+//     function(){
+//         $(this).css("color", "black");
+//
+// })
