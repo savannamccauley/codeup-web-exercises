a3f89d750fb577e4e60e75a923e78028c88f29a3
Savanna McCauley <savannamccauley@yahoo.com>
2023-04-03 16:47:51 -0500

    weather map

--- js/mapbox-geocoder.js
+++ js/mapbox-geocoder.js
@@ -1,16 +1,14 @@
-// function geocode(search, token) {
-//     var baseUrl = 'https://api.mapbox.com';
-//     var endPoint = '/geocoding/v5/mapbox.places/';
-//     return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
-//         .then(function(res) {
-//             return res.json();
-//             // to get all the data from the request, comment out the following three lines...
-//         }).then(function(data) {
-//             return data.features[0].center;
-//         });
-// }
-//
+function geocode(search, token) {
+    var baseUrl = 'https://api.mapbox.com';
+    var endPoint = '/geocoding/v5/mapbox.places/';
+    return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
+        .then(function(res) {
+            return res.json();
+            // to get all the data from the request, comment out the following three lines...
+        }).then(function(data) {
+            return data.features[0].center;
+        });
+}
 
-const MAPBOX_TOKEN = "pk.eyJ1Ijoic21jY2F1bGV5MTgiLCJhIjoiY2xmdjdiZzVuMDRibTNsbzNmZndldWVqaiJ9.ibpOdpfwfmrwtVcqHVYlDQ";
 
-console.log("hello");
+
